d041f6d777cac4500c6fde3d4fab738f9b286e5e
smallville7123 <smallville7123@gmail.com>
2023-12-02 21:23:56 +1000

    update linux workflow

--- install_systemd.sh
+++ install_systemd.sh
@@ -26,13 +26,13 @@ sudo docker container start $CONTAINER
 sudo docker container exec $CONTAINER bash -c "sleep 5 ; systemctl --wait is-system-running || sysctl --failed"
 sudo docker container exec $CONTAINER bash -c "echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ubuntu"
 echo "creating user session"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo mkdir -p /run/user/$(id -u ubuntu)"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo chown ubuntu:ubuntu /run/user/$(id -u ubuntu)"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo systemctl start user@$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo mkdir -p /run/user/\$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo chown ubuntu:ubuntu /run/user/\$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo systemctl start user@\$(id -u ubuntu)"
 echo "created user session"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --system\""
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --user\""
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"sudo apt install -y polkitd pkexec polkitd-pkla\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --system\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --user\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"sudo apt install -y polkitd pkexec polkitd-pkla\""
 echo commiting to image
 sudo docker container commit $CONTAINER git_local_ubuntu-systemd-polkitd:23.10
 echo stopping
@@ -47,13 +47,13 @@ echo starting
 sudo docker container start $CONTAINER
 sudo docker container exec $CONTAINER bash -c "sleep 5 ; systemctl --wait is-system-running || sysctl --failed"
 echo "creating user session"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo mkdir -p /run/user/$(id -u ubuntu)"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo chown ubuntu:ubuntu /run/user/$(id -u ubuntu)"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo systemctl start user@$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo mkdir -p /run/user/\$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo chown ubuntu:ubuntu /run/user/\$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo systemctl start user@\$(id -u ubuntu)"
 echo "created user session"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --system\""
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --user\""
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"sudo apt install -y flatpak flatpak-builder\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --system\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --user\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"sudo apt install -y flatpak flatpak-builder\""
 echo commiting to image
 sudo docker container commit $CONTAINER git_local_ubuntu-systemd-polkitd-flatpak:23.10
 echo stopping
@@ -68,14 +68,14 @@ echo starting
 sudo docker container start $CONTAINER
 sudo docker container exec $CONTAINER bash -c "sleep 5 ; systemctl --wait is-system-running || sysctl --failed"
 echo "creating user session"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo mkdir -p /run/user/$(id -u ubuntu)"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo chown ubuntu:ubuntu /run/user/$(id -u ubuntu)"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo systemctl start user@$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo mkdir -p /run/user/\$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo chown ubuntu:ubuntu /run/user/\$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo systemctl start user@\$(id -u ubuntu)"
 echo "created user session"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --system\""
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --user\""
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo\""
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"sudo flatpak install -y flathub org.freedesktop.Platform//23.08 org.freedesktop.Sdk//23.08\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --system\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --user\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"sudo flatpak install -y flathub org.freedesktop.Platform//23.08 org.freedesktop.Sdk//23.08\""
 echo commiting to image
 sudo docker container commit $CONTAINER git_local_ubuntu-systemd-polkitd-flatpak:23.10
 echo stopping
@@ -92,18 +92,18 @@ sudo docker container exec $CONTAINER bash -c "sleep 5 ; systemctl --wait is-sys
 sudo docker container exec $CONTAINER bash -c "systemctl status --system"
 sudo docker container exec $CONTAINER bash -c "echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ubuntu"
 echo "creating user session"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo mkdir -p /run/user/$(id -u ubuntu)"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo chown ubuntu:ubuntu /run/user/$(id -u ubuntu)"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo systemctl start user@$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo mkdir -p /run/user/\$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo chown ubuntu:ubuntu /run/user/\$(id -u ubuntu)"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu $CONTAINER bash -c "sudo systemctl start user@\$(id -u ubuntu)"
 echo "created user session"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --system\""
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --user\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --system\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"systemctl status --user\""
 cat << EOF > install_systemd_tmp.sh
 set -e
 sudo docker cp flat.sh $CONTAINER:/home/ubuntu/flat.sh
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"chmod +x flat.sh ; ./flat.sh\""
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -c \"chmod +x flat.sh ; ./flat.sh\""
 echo "launch interactive shell"
-sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -i"
+sudo docker container exec --user ubuntu --workdir /home/ubuntu -it $CONTAINER bash -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u ubuntu)/bus systemd-run --user --scope --collect /bin/bash -i"
 EOF
 chmod +x install_systemd_tmp.sh
 ./install_systemd_tmp.sh || true
